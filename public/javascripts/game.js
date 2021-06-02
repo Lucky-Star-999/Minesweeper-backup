5a456ec1e5bf0994787ba3c9253d85042156a6be
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-06-02 11:15:16 +0700

    Update UI

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -8,7 +8,6 @@ var info_numberofbombs = document.getElementById('final-numberofbombs');
 var final_time_elapsed = document.getElementById('final-time-elapsed');
 var does_need_to_publish = document.getElementById('does_need_to_publish');
 
-
 App_Operator.player_name = info_playername.innerHTML;
 App_Operator.squares_in_a_row = parseInt(document.getElementById('row_length').innerHTML);
 App_Operator.squares_in_a_column = parseInt(document.getElementById('column_length').innerHTML);
@@ -21,14 +20,7 @@ if (does_need_to_publish.innerHTML === "true") {
 }
 
 App_Operator.active_undo_button();
-
 Board_Making.create_grid();
-
-
-//Test get data from API
-//Board_Making.get_board_from_api();
-
-
 
 
 /******************************************** Time counter ********************************************************/
@@ -47,12 +39,12 @@ function time_counter() {
                 clearInterval(interval_time_id);
                 if(App_Operator.game_state === "Lose"){
                     alert("You lose!");
-                }else if(App_Operator.game_state === "Win"){
+                } else if(App_Operator.game_state === "Win"){
                     alert("You win!");
                 }
                 if(App_Operator.is_guest){
 
-                }else{
+                } else {
                     App_Operator.upload_to_leaderboard();
                 }
                 
